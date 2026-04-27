abacba35b32e412de0d3f3e2bc76660fad030321
Axelle <axelle.frini@competitivia.com>
2026-04-27 08:46:31 +0100

    feat: update Home component to include Activation, Testimonials, Difference, FinalCTA, and Footer sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Activation } from "@/components/activation";
+import { FinalCTA } from "@/components/cta";
+import { Difference } from "@/components/difference";
+import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/header";
 import { Hero } from "@/components/hero";
 import { Pricing } from "@/components/pricing";
 import { Problem } from "@/components/problem";
 import { Solution } from "@/components/solution";
+import { Testimonials } from "@/components/tesimonials";
 
 export default function Home() {
   return (
@@ -14,6 +19,12 @@ export default function Home() {
       <Problem />
       <Solution />
       <Pricing />
+      <Activation />
+      <Testimonials />
+      <Difference/>
+      <FinalCTA/>
+      <Footer/> 
+
 
     </main>
   );
